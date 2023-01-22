93d814790e79518abc0a99a60b00dfc8b10c734c
Jonathan Oregon <oregonjonathan@gmail.com>
2023-01-22 19:49:40 +0000

    add time vars

--- time_calculator.py
+++ time_calculator.py
@@ -1,6 +1,10 @@
-def add_time(start, duration):
-
-
+#start time in the 12-hour clock format (ending in AM or PM)
+#a duration time that indicates the number of hours and minutes
+#(optional) a starting day of the week, case insensitive
+def add_time(start, duration, *arg):
+  start = start.replace(' ', ':').split(':')
+  h, m, end = start[0], start[1], start[2]
+  
 
 
 
