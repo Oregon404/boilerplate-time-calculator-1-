7092f9f8b73d79ecb994b778d72b6edfb6cddbf8
Jonathan Oregon <oregonjonathan@gmail.com>
2023-01-23 06:31:16 +0000

    fix "days later"

--- time_calculator.py
+++ time_calculator.py
@@ -1,11 +1,31 @@
 #start time in the 12-hour clock format (ending in AM or PM)
 #a duration time that indicates the number of hours and minutes
 #(optional) a starting day of the week, case insensitive
-def add_time(start, duration, *arg):
+def add_time(start, duration, *args):
   start = start.replace(' ', ':').split(':')
-  h, m, end = start[0], start[1], start[2]
-  
-
-
-
-    return new_time
+  duration = duration.split(':')
+  t = (int(start[1]) + int(duration[1])) // 60
+  m = (int(start[1]) + int(duration[1])) % 60
+  h = (int(start[0]) + int(duration[0]) + t)
+  end, day = start[2], ''
+  if end == 'PM':
+    h += 12
+  t = h // 24
+  if (h % 24) - 12 >= 0:
+    end = 'PM'
+  else:
+    end = 'AM'
+  h = h % 12
+  if h == 0:
+    h = 12
+  if args:
+    week = [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+      'Saturday'
+    ]
+    day = f', {week[(week.index(str(args[0]).title()) + t) % 7]}'
+  if t == 1:
+    day = day + ' (next day)'
+  if t > 1:
+    day = day + f' ({t} days later)'
+  return (f'{h}:{m:02d} {end}{day}')
